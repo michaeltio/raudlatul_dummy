e3c65984b1f3fb5be3851b067a8ad2c4ded2eaae
H0tt4ru <stenlieangelonathan@gmail.com>
2024-08-29 22:20:06 +0700

    First Commit

---
+++ client/src/api/apiClient.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:3001/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API call failed:", error);
+    if (error.response.status === 401) {
+    } else if (error.response.status === 404) {
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
