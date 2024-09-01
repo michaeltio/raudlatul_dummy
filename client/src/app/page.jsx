e1877909a6968d90a0374569969388f9d4a70e8e
acd <amanda.acd29@gmail.com>
2024-09-01 19:04:07 +0700

    1

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,24 +1,24 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import apiClient from "@/api/apiClient";
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import apiClient from "@/api/apiClient";
 
-export default function Home() {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await apiClient.get("/");
-        console.log(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+// export default function Home() {
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await apiClient.get("/");
+//         console.log(response.data);
+//       } catch (error) {
+//         console.error("Error fetching data:", error);
+//       }
+//     };
 
-    fetchData();
-  }, []);
+//     fetchData();
+//   }, []);
 
-  return (
-    <>
-      <section>Test</section>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <section>Test</section>
+//     </>
+//   );
+// }
