2eb0cbaa7dfa205636deea8b7459daabcbec3740
H0tt4ru <stenlieangelonathan@gmail.com>
2024-09-05 12:06:54 +0700

    Update Footer.jsx

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -3,14 +3,19 @@ export default function Footer() {
     <div className="mt-8">
       <div className="text-xxs mx-6 flex flex-col items-center gap-6 border-b-2 border-t-2 border-[#092928] py-4 font-ptserif text-xs font-semibold">
         <h1>Raudlatul Irfan</h1>
-        <div className="flex gap-6">
+        <div className="flex gap-4">
           <div className="h-4 w-4 rounded-full bg-[#092928]"></div>
           <div className="h-4 w-4 rounded-full bg-[#092928]"></div>
           <div className="h-4 w-4 rounded-full bg-[#092928]"></div>
         </div>
         <div className="flex flex-col items-center">
+<<<<<<< Updated upstream
           <h1 className="text-xxxs">Lorem Ipsum</h1>
           <div className="text-xxxxs flex flex-col font-normal">
+=======
+          <h1 className="text-3xs">Lorem Ipsum</h1>
+          <div className="text-4xs flex flex-col -space-y-2 font-normal">
+>>>>>>> Stashed changes
             <p>Lorem Ipsum</p>
             <p>Lorem Ipsum</p>
             <p>Lorem Ipsum</p>
