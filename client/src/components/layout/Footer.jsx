17ed7be57b6c9fd400731ab2079b7dea0f8d9679
H0tt4ru <stenlieangelonathan@gmail.com>
2024-09-09 10:39:28 +0700

    PreMerge

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="mt-8">
+    <div className="bottom-0 mt-8">
       <div className="text-2xs mx-6 flex flex-col items-center gap-6 border-b-2 border-t-2 border-[#092928] py-4 font-ptserif text-xs font-semibold">
         <h1>Raudlatul Irfan</h1>
         <div className="flex gap-4">
@@ -10,7 +10,7 @@ export default function Footer() {
         </div>
         <div className="flex flex-col items-center">
           <h1 className="text-3xs">Lorem Ipsum</h1>
-          <div className="text-4xs flex flex-col font-normal">
+          <div className="text-4xs flex flex-col -space-y-2 font-normal">
             <p>Lorem Ipsum</p>
             <p>Lorem Ipsum</p>
             <p>Lorem Ipsum</p>
