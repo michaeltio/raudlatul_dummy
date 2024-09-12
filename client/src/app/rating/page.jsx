7c2eea36f93c46003cc32d619c36db1cd493c0e6
WarmHatori <warmhatori@gmail.com>
2024-09-12 21:33:40 +0700

    rating

--- client/src/app/rating/page.jsx
+++ client/src/app/rating/page.jsx
@@ -21,14 +21,14 @@ export default function Service() {
           </button>
         </div>
         <Rating
-          img="tester.webp"
+          img="caligraphy01.webp"
           title="Kaligrafi Surah Yasin 20x40"
           p1="Total 1 Product:"
           p2="Rp 1.250.000"
           p3="1x"
         />
         <Rating
-          img="tester.webp"
+          img="caligraphy01.webp"
           title="Kaligrafi Surah Al-Fatihah 40x40"
           p1="Total 2 Product:"
           p2="Rp 4.250.000"
