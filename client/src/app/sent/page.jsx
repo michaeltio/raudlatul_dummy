540e2004612e98ca4619976401e65b1907549209
WarmHatori <warmhatori@gmail.com>
2024-09-12 21:36:37 +0700

    sent

--- client/src/app/sent/page.jsx
+++ client/src/app/sent/page.jsx
@@ -11,8 +11,8 @@ export default function Service() {
                 <button className="bg-[#E9B472] p-1 w-32 rounded-3xl font-ptserif font-black text-2xl text-[#FAF1EA] hover:bg-[#C6975D]">Sent</button>
                 <button className="bg-[#E9B472] p-1 w-32 rounded-3xl font-ptserif font-black text-2xl text-[#FAF1EA] hover:bg-[#C6975D]">Rating</button>
             </div>
-            <Sent img="tester.webp" title="Kaligrafi Surah Yasin 20x40" p1="Total 1 Product:" p2="Rp 1.250.000" p3="1x"/>
-            <Sent img="tester.webp" title="Kaligrafi Surah Al-Fatihah 40x40" p1="Total 2 Product:" p2="Rp 4.250.000" p3="2x"/>
+            <Sent img="caligraphy01.webp" title="Kaligrafi Surah Yasin 20x40" p1="Total 1 Product:" p2="Rp 1.250.000" p3="1x"/>
+            <Sent img="caligraphy01.webp" title="Kaligrafi Surah Al-Fatihah 40x40" p1="Total 2 Product:" p2="Rp 4.250.000" p3="2x"/>
         </div>
     </>
   );
