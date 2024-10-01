9a07bf019827be7a3d39e6a0a286f533fc97bbc2
WarmHatori <warmhatori@gmail.com>
2024-10-02 00:38:58 +0700

    up

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -18,4 +18,5 @@ apiClient.interceptors.response.use(
   }
 );
 
+
 export default apiClient;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,139 @@
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const {
+//   initializeFirebaseApp,
+//   uploadProccessedData,
+//   getCollectionData,
+//   getCollectionDataWhere,
+//   editCollectionData,
+//   deleteCollectionData,
+//   createUser,
+//   signInUser,
+//   signOutUser,
+// } = require("./firebase");
+
+// const PORT = 3001;
+// initializeFirebaseApp();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Authentication
+// app.post("/register", async (req, res) => {
+//   const { email, password } = req.body;
+//   const user = await createUser(email, password);
+//   return res.json(user);
+// });
+
+// app.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+//   const user = await signInUser(email, password);
+//   return res.json(user);
+// });
+
+// app.post("/logout", async (req, res) => {
+//   const user = await signOutUser();
+//   return res.json(user);
+// });
+
+// // Data
+
+// app.get("/api/item", async (req, res) => {
+//   const data = await getCollectionData("KaligraphyItem");
+//   return res.json(data);
+// });
+
+// app.get("/api/achievement", async (req, res) => {
+//   const data = await getCollectionData("Achievement");
+//   return res.json(data);
+// });
+
+// app.get("/api/courier", async (req, res) => {
+//   const data = await getCollectionData("Courier");
+//   return res.json(data);
+// });
+
+// app.get("/api/customer", async (req, res) => {
+//   const data = await getCollectionData("Customer");
+//   return res.json(data);
+// });
+
+// app.get("/api/order", async (req, res) => {
+//   const data = await getCollectionData("Order");
+//   return res.json(data);
+// });
+
+// app.get("/api", async (req, res) => {
+//   const data = await getCollectionDataWhere(
+//     "KaligraphyItem",
+//     "item_name",
+//     "ex_name"
+//   );
+//   return res.json(data);
+// });
+
+// app.get("/api/reviews", async (req, res) => {
+//   const data = await getCollectionData("Review");
+//   return res.json(data);
+// });
+
+// app.post("/delete/item", async (req, res) => {
+//   const { id } = req.body;
+//   await deleteCollectionData("KaligraphyItem", id);
+//   return res.json({ message: "Data deleted successfully!" });
+// });
+
+// app.post("/delete/order", async (req, res) => {
+//   const { id } = req.body;
+//   await deleteCollectionData("Order", id);
+//   return res.json({ message: "Data deleted successfully!" });
+// });
+
+// app.post("/edit/item", async (req, res) => {
+//   const { id, data } = req.body;
+//   const updatedData = await editCollectionData("KaligraphyItem", id, data);
+//   return res.json({ message: "Data edited successfully!" });
+// });
+
+// app.post("/edit/reviews", async (req, res) => {
+//   const { id, data } = req.body;
+//   const updatedData = await editCollectionData("Review", id, data);
+//   return res.json({ message: "Data edited successfully!" });
+// });
+
+// app.post("/create/add-item", async (req, res) => {
+//   await uploadProccessedData(req.body, "KaligraphyItem");
+//   return res.json({ message: "Data uploaded successfully!" });
+// });
+
+// app.post("/create/add-courier", async (req, res) => {
+//   await uploadProccessedData(req.body, "Courier");
+//   return res.json({ message: "Data uploaded successfully!" });
+// });
+
+// app.post("/create/add-customer", async (req, res) => {
+//   await uploadProccessedData(req.body, "Customer");
+//   return res.json({ message: "Data uploaded successfully!" });
+// });
+
+// app.post("/create/ratings", async (req, res) => {
+//   await uploadProccessedData(req.body, "Review");
+//   return res.json({ message: "Data uploaded successfully!" });
+// });
+
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const {
   initializeFirebaseApp,
-  uploadProccessedData,
-  getCollectionData,
-  getCollectionDataWhere,
-  editCollectionData,
-  deleteCollectionData,
-  createUser,
-  signInUser,
-  signOutUser,
+  postData,
+  getAllData,
+  getData,
+  updateData,
+  deleteData,
 } = require("./firebase");
 
 const PORT = 3001;
@@ -19,107 +142,39 @@ initializeFirebaseApp();
 app.use(cors());
 app.use(express.json());
 
-// Authentication
-app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await createUser(email, password);
-  return res.json(user);
+app.post("/create/:category", async (req, res) => {
+  const category = req.params.category;
+  await postData(req.body, category);
+  return res.json({ message: "Data uploaded successfully!" });
 });
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await signInUser(email, password);
-  return res.json(user);
-});
+app.get("/read/:category", async (req, res) => {
+  const category = req.params.category;
+  const data = await getAllData(category);
 
-app.post("/logout", async (req, res) => {
-  const user = await signOutUser();
-  return res.json(user);
-});
-
-// Data
-
-app.get("/api/item", async (req, res) => {
-  const data = await getCollectionData("KaligraphyItem");
   return res.json(data);
 });
 
-app.get("/api/achievement", async (req, res) => {
-  const data = await getCollectionData("Achievement");
+app.get("/read/:category/:id", async (req, res) => {
+  const category = req.params.category;
+  const id = req.params.id;
+  const data = await getData(category, id);
+
   return res.json(data);
 });
 
-app.get("/api/courier", async (req, res) => {
-  const data = await getCollectionData("Courier");
-  return res.json(data);
-});
-
-app.get("/api/customer", async (req, res) => {
-  const data = await getCollectionData("Customer");
-  return res.json(data);
-});
-
-app.get("/api/order", async (req, res) => {
-  const data = await getCollectionData("Order");
-  return res.json(data);
-});
-
-app.get("/api", async (req, res) => {
-  const data = await getCollectionDataWhere(
-    "KaligraphyItem",
-    "item_name",
-    "ex_name"
-  );
-  return res.json(data);
-});
-
-app.get("/api/reviews", async (req, res) => {
-  const data = await getCollectionData("Review");
-  return res.json(data);
-});
-
-app.post("/delete/item", async (req, res) => {
-  const { id } = req.body;
-  await deleteCollectionData("KaligraphyItem", id);
-  return res.json({ message: "Data deleted successfully!" });
-});
-
-app.post("/delete/order", async (req, res) => {
-  const { id } = req.body;
-  await deleteCollectionData("Order", id);
-  return res.json({ message: "Data deleted successfully!" });
-});
-
-app.post("/edit/item", async (req, res) => {
-  const { id, data } = req.body;
-  const updatedData = await editCollectionData("KaligraphyItem", id, data);
+app.post("/update/:category/:id", async (req, res) => {
+  const category = req.params.category;
+  const id = req.params.id;
+  await updateData(category, id, req.body);
   return res.json({ message: "Data edited successfully!" });
 });
 
-app.post("/edit/reviews", async (req, res) => {
-  const { id, data } = req.body;
-  const updatedData = await editCollectionData("Review", id, data);
-  return res.json({ message: "Data edited successfully!" });
-});
-
-app.post("/create/add-item", async (req, res) => {
-  await uploadProccessedData(req.body, "KaligraphyItem");
-  return res.json({ message: "Data uploaded successfully!" });
-});
-
-app.post("/create/add-courier", async (req, res) => {
-  await uploadProccessedData(req.body, "Courier");
-  return res.json({ message: "Data uploaded successfully!" });
-});
-
-app.post("/create/add-customer", async (req, res) => {
-  await uploadProccessedData(req.body, "Customer");
-  return res.json({ message: "Data uploaded successfully!" });
-});
-
-app.post("/create/ratings", async (req, res) => {
-  await uploadProccessedData(req.body, "Review");
-  return res.json({ message: "Data uploaded successfully!" });
+app.post("/delete/:category/:id", async (req, res) => {
+  const category = req.params.category;
+  const id = req.params.id;
+  await deleteData(category, id);
+  return res.json({ message: "Data deleted successfully!" });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
