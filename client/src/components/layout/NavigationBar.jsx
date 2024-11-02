129dd5f426281ff20103ab35cf946e16ad9d7649
Nathan Angelo Stenlie <stenlieangelonathan@gmail.com>
2024-11-02 14:32:48 +0700

    update

--- client/src/components/layout/NavigationBar.jsx
+++ client/src/components/layout/NavigationBar.jsx
@@ -12,8 +12,8 @@ export default function NavigationBar() {
 
   return (
     <>
-      <nav className="z-50 bg-primaryWhite bg-opacity-50 backdrop-blur-sm fixed flex w-screen flex-row items-center justify-between px-6 py-4 shadow-[rgba(0,0,15,0.25)_0px_5px_5px_0px] md:px-12 md:py-6">
-        <h1 className="font-ptserif text-2xs font-semibold tracking-wide md:text-lg">
+      <nav className="fixed z-50 flex w-screen flex-row items-center justify-between bg-primaryWhite bg-opacity-50 px-6 py-4 shadow-[rgba(0,0,15,0.25)_0px_5px_5px_0px] backdrop-blur-sm md:px-12 md:py-6">
+        <h1 className="text-2xs font-ptserif font-semibold tracking-wide md:text-lg">
           Raudlatul Irfan
         </h1>
         <button
@@ -30,13 +30,13 @@ export default function NavigationBar() {
           />
         </button>
         <div className="hidden items-center gap-12 md:flex">
-          <Link href="">
+          <Link href="/">
             <h1 className={`inline border-b-2 border-[#E9B472]`}>Home</h1>
           </Link>
           <Link href="/service">
             <h1 className={`inline border-b-2 border-[#E9B472]`}>Service</h1>
           </Link>
-          <Link href="/about">
+          <Link href="/about-us">
             <h1 className={`inline border-b-2 border-[#E9B472]`}>About</h1>
           </Link>
           <Link href="/shop">
@@ -75,7 +75,7 @@ export default function NavigationBar() {
               </button>
               <div className="flex flex-col gap-8">
                 <div className="mt-24 flex flex-col gap-4">
-                  <Link href="">
+                  <Link href="/">
                     <h1 className={`inline border-b-2 border-[#E9B472]`}>
                       Home
                     </h1>
@@ -85,7 +85,7 @@ export default function NavigationBar() {
                       Service
                     </h1>
                   </Link>
-                  <Link href="/about">
+                  <Link href="/about-us">
                     <h1 className={`inline border-b-2 border-[#E9B472]`}>
                       About
                     </h1>
