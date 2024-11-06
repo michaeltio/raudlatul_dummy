7b9f113d18dc45b791dd87c0637a08431a95e2d0
acd <amanda.citra@student.umn.ac.id>
2024-11-06 11:22:29 +0700

    up button

--- client/src/components/shop/SearchBar.jsx
+++ client/src/components/shop/SearchBar.jsx
@@ -12,17 +12,6 @@ export default function SearchBar({ item }) {
         className="h-8 w-full rounded-full border border-black bg-transparent p-2 px-6 md:h-10"
       />
       <Link
-        href="/wishlist"
-        className="aspect-square h-8 rounded-full bg-[#E9B472] hover:bg-[#C6975D] md:h-10"
-      >
-        <Image
-          src="/svg/icon/love.svg"
-          width={100}
-          height={100}
-          className="w-full p-2"
-        />
-      </Link>
-      <Link
         href="/cart"
         className="aspect-square h-8 rounded-full bg-[#092928] hover:bg-black md:h-10"
       >
