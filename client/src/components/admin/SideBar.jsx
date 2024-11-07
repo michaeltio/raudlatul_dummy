e1b51f58c6d423edb9225d503cadb625d68113de
WarmHatori <warmhatori@gmail.com>
2024-11-07 19:34:24 +0700

    Update SideBar.jsx

--- client/src/components/admin/SideBar.jsx
+++ client/src/components/admin/SideBar.jsx
@@ -44,7 +44,9 @@ export default function SideBar() {
 
       <aside
         id="cta-button-sidebar"
-        class="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full transition-transform sm:translate-x-0"
+        className={`fixed left-0 top-0 z-40 h-screen w-64 transition-transform sm:translate-x-0 ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
         aria-label="Sidebar"
       >
         <div class="h-full overflow-y-auto bg-gray-50 px-3 py-4 dark:bg-[#BFCFBB]">
