1856d420ae319d65b3bd1d2f0616bfc74b11222e
Nathan Angelo Stenlie <stenlieangelonathan@gmail.com>
2024-11-10 17:21:42 +0700

    Host Ready

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://utsmichaeltio70515.xyz",
   headers: {
     "Content-Type": "application/json",
   },
